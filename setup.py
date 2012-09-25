aaaeabb9741087a631af09c21c90bd313c882015
Francesco Facconi <francesco@facconi.eu>
2012-09-25 08:38:44 +0200

    Distutils configuration

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='cmsplugin_vimeo',
+    version='1.0',
+    packages=['cmsplugin_vimeo', 'cmsplugin_vimeo.migrations'],
+    url='https://github.com/Immediatic/cmsplugin_vimeo',
+    license='LICENSE.txt',
+    author='Francesco Facconi',
+    author_email='francesco@immediatic.it',
+    description='Plugin for Django CMS that makes easy to embed Vimeo movies.'
+)
